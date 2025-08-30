5446158d8096da7e9afcccb68f47650ad48803e2
Jacob-Hawksley <jacobmhawksley@gmail.com>
2025-08-30 11:43:58 +0100

    Added bullet lifespan to prevent lag

--- bullet.gd
+++ bullet.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 var speed = 2000
 var damage = 10
+var lifespan = 5
+func _process(delta: float) -> void:
+	lifespan -= delta
+	if lifespan <= 0:
+		queue_free()
 func _physics_process(delta):
 	position -= transform.x * speed * delta
 
@@ -9,3 +14,5 @@ func _physics_process(delta):
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method('hurt'):
 		body.hurt(damage)
+	else:
+		queue_free()
--- shell.gd
+++ shell.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 var speed = 4000
 var lifespan = 0.1
+var damage = 3
 func _physics_process(delta):
 	position -= transform.x * speed * delta
 	lifespan -= delta
 	if lifespan <= 0:
 		queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method('hurt'):
+		body.hurt(damage)
